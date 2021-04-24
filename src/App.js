c9a2802f8bfbe71d5c14268b0a86a02a22cd46db
Rodrigo <rd.ferrari@gmail.com>
2021-04-25 10:08:10 +1200

    React lazy import

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import styled from "styled-components";
 import "./App.css";
 // Components
-import Header from "./Header";
-import SectionOne from "./SectionOne";
-import SectionThree from "./SectionThree";
-import SectionTwo from "./SectionTwo";
+
+const Header = lazy(() => import("./Header"));
+const SectionOne = lazy(() => import("./SectionOne"));
+const SectionTwo = lazy(() => import("./SectionTwo"));
+const SectionThree = lazy(() => import("./SectionThree"));
 
 const Footer = styled.img`
   right: 0;
@@ -14,13 +15,51 @@ const Footer = styled.img`
   z-index: 11;
 `;
 
+const LoadingContainer = styled.div`
+  width: 100%;
+  padding: 50px;
+  text-align: center;
+`;
+
 function App() {
   return (
     <div>
-      <Header />
-      <SectionOne />
-      <SectionTwo />
-      <SectionThree />
+      <Suspense
+        fallback={
+          <LoadingContainer>
+            <img src="/loading.svg" alt="Loading" />
+          </LoadingContainer>
+        }
+      >
+        <Header />
+      </Suspense>
+      <Suspense
+        fallback={
+          <LoadingContainer>
+            <img src="/loading.svg" alt="Loading" />
+          </LoadingContainer>
+        }
+      >
+        <SectionOne />
+      </Suspense>
+      <Suspense
+        fallback={
+          <LoadingContainer>
+            <img src="/loading.svg" alt="Loading" />
+          </LoadingContainer>
+        }
+      >
+        <SectionTwo />
+      </Suspense>
+      <Suspense
+        fallback={
+          <LoadingContainer>
+            <img src="/loading.svg" alt="Loading" />
+          </LoadingContainer>
+        }
+      >
+        <SectionThree />
+      </Suspense>
       <Footer alt="Flow logo" src="/footer.svg" />
     </div>
   );
